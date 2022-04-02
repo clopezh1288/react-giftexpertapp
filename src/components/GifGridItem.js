be1b23f7648e08afa36e9831dea0a1dc02e012a2
Carlos Lopez <carlos.lopez@develapp.us>
2022-04-02 15:37:11 -0600

    First commit

---
+++ src/components/GifGridItem.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export const GifGridItem = ({ title, url }) => {
+
+    return (
+        <div className='card animate__animated animate__fadeIn'>
+            <img
+                alt={title}
+                src={url}
+            />
+            <p>{title}</p>
+        </div>
+    )
+}
